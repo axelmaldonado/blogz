7869052d84cfbc00d8dda19fdc5e501c93e85e43
Axel Maldonado <axel@axelmaldonado.com>
2018-11-15 01:16:18 -0600

    Implemented up to /Home

--- main2.py
+++ main2.py
@@ -1,21 +1,22 @@
 from flask import Flask, request, redirect, render_template, url_for, session, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:admin@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz2:admin@localhost:8889/blogz2'
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.secret_key = 'W3YnjNA&kuMJK'
-
+app.secret_key = 'djfjfeuhs98x'
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(400))
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(500))
     deleted = db.Column(db.Boolean, default=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
     def __init__(self, title, body, owner):
         self.title = title
@@ -34,107 +35,93 @@ class User(db.Model):
         self.username = username
         self.password = password
 
-    # existing_user = User.query.filter_by(username=username).first()
-
-# This is not a request handler. We want this to run for every request
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'signup', 'blogs', 'index']
+    allowed_routes = ['login','signup','blogs','index']
     if request.endpoint not in allowed_routes and 'username' not in session:
-        return redirect('/login')
+        return redirect("/login")
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route("/login",methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
-        
-        #user enters a username that is stored in the db with
-        #the correct password and is redirected to the /newpost page
-        #with their username being stored in a session.
         if user and user.password == password:
             session['username'] = username
-            flash("Logged in")
-            return redirect('/')
-        #User enters a username that is stored in the database with an 
-        # incorrect password and is redirected to the /login page with 
-        # a message that their password is incorrect.
-        if user and user.password != password:
-            flash("Password Incorrect", "error")
-            return redirect('/login')
-        #User tries to login with a username that is not stored in the database and is 
-        # redirected to the /login page with a message that this username does not exist.      
-        if not user:
-            flash('Username does not exist', 'error')
-            return redirect('/login')
+            flash('Logged In')
+            return redirect('/newpost')
+        else:
+            flash("Username or Password incorrect, or User does not exist.", "error")
+            return redirect("/login")
 
-    return render_template('login.html')
+    return render_template("login.html")
 
-
-@app.route('/signup', methods=['POST', 'GET'])
+@app.route("/signup", methods=['GET','POST'])
 def signup():
+
     if request.method == 'POST':
+
         username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
         username_error = False
         password_error = False
         verify_error = False
-        
-        if username == "":
-            flash("Username cannot be blank", 'error')
-            username_error = True
-        if len(username) < 3:
-            flash("Username must be greater than 3 characters", 'error')
-            username_error = True
-        
-        if password == "":
-            flash("Password cannot be blank", 'error')
-            password_error = True
-        if len(username) < 3:
-            flash("Password must be greater than 3 characters", 'error')
-            password_error = True
 
+        if username == "":
+            flash("Please enter a username.","error")
+            username_error = True
+        if len(username) < 3:
+            flash("Please enter a valid username that is more than three characters long.","error")
+            username_error = True
+        if password == "":
+            flash("Please enter a valid password","error")
+            password = ""
+            verify = ""
+            password_error = True
         if password != verify:
-            flash("Passwords don't match", 'error')
+            flash("Your password did not match in the verification field. " + 
+            "Please ensure your password matches in both fields.","error")
+            password = ""
+            verify = ""
             verify_error = True
+        if len(password) < 3:
+            flash("Please enter a valid password that is more than three characters long.","error")
+            password_error = True
 
         if not username_error and not password_error and not verify_error:
             existing_user = User.query.filter_by(username=username).first()
             if not existing_user:
-                new_user = User(username, password)
+                new_user = User(username,password)
                 db.session.add(new_user)
                 db.session.commit()
                 session['username'] = username
                 return redirect("/newpost")
             else:
-                flash("Username already taken", "error")
+                flash("Username already taken.")
         else:
             return redirect("/signup")
 
     return render_template("signup.html")
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     del session['username']
     flash("Logged Out")
-    return redirect('/login')
+    return redirect("/blog")
 
 @app.route("/")
 def index():
-    user_id = request.args.get("user")
     users = User.query.all()
-    posts = Blog.query.filter_by(owner_id=user_id).all()
-    return render_template("index.html", users=users, posts=posts, title="Blog Users")
-
+    return render_template("index.html",users=users)
 
 @app.route("/blog", methods=["GET","POST"])
 def blogs():
 
     id_check = request.args.get("id")
     user_check = request.args.get("user")
-    posts = Blog.query.all()
+    posts = Blog.query.filter_by(deleted=False).all()
 
     if id_check is not None:
         post_id = int(request.args.get("id"))
@@ -143,13 +130,12 @@ def blogs():
     if user_check is not None:
         user_id = request.args.get("user")
         posts = Blog.query.filter_by(owner_id=user_id).all()
-        return render_template("SingleUser.html",posts=posts)
+        return render_template("user.html",posts=posts)
     else:
         posts = Blog.query.filter_by(deleted=False).all()
         
 
-    return render_template("index.html", posts=posts, title="Blog Posts")
-
+    return render_template("index.html", posts=posts)
 
 @app.route("/newpost", methods=['GET','POST'])
 def add_blog():
@@ -187,6 +173,6 @@ def add_blog():
 
     return render_template("add-blog.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
 
